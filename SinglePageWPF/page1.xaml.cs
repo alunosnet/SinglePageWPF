1e2a8aa5e7239c23444cce5666c9ad7ff53bd9b2
Paulo Ferreira <alunosnet@gmail.com>
2020-12-18 10:38:06 +0000

    Add project files.

---
+++ SinglePageWPF/page1.xaml.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace SinglePageWPF
+{
+    /// <summary>
+    /// Interaction logic for page1.xaml
+    /// </summary>
+    public partial class page1 : Page
+    {
+        public page1()
+        {
+            InitializeComponent();
+        }
+    }
+}
